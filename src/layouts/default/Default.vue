eb8f29c9ccb0da4bd0c11d9d18a7003b827f7b2d
Upepo Mwindaji <upepo.mwindaji@gmail.com>
2023-01-02 22:38:08 +0100

    boilerplate clean-up

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-title> VUEGAR DEMO </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
     <default-view />
   </v-app>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <h1> Demo </h1>
+    <h2> Get Accounts </h2>
+    <h2> Get Segments </h2>
+    <h2> Get Report </h2>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
